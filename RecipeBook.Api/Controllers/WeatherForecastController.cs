43e951412276bc1d931615bc90d35e8d8a533c5f
Colton Randall <coltonrandall@Coles-Air.hub>
2024-07-18 20:07:03 +1200

    add docker-compose config

--- RecipeBook.Api/Controllers/WeatherForecastController.cs
+++ RecipeBook.Api/Controllers/WeatherForecastController.cs
@@ -12,21 +12,29 @@ public class WeatherForecastController : ControllerBase
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
+    private readonly IConfiguration _configuration;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration configuration)
     {
         _logger = logger;
+        _configuration = configuration;
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+    }
+
+    [HttpGet("apikey")]
+    public string GetApiKey()
+    {
+        return _configuration.GetValue<string>("ApiKey");
     }
 }
